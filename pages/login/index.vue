853494754c58c224d88c1e9fbdc90355eee0ef23
zhangyuchi <zhangyuchi@polyihome.com>
2020-10-09 11:14:19 +0800

    首次提交

---
+++ pages/login/index.vue
@@ -0,0 +1,91 @@
+<template>
+	<view class="page">
+		<view class="content">
+			<view class="form-item-title">
+				爱家经纪云
+			</view>
+			<view class="form-item form-item-buttom">
+				<view class="form-item-left">账号</view>
+				<view class="form-item-right">
+					<input placeholder="账号" />
+				</view>
+			</view>
+			<view class="form-item form-item-buttom">
+				<view class="form-item-left">密码</view>
+				<view class="form-item-right">
+					<input type="text" password placeholder="密码" />
+				</view>
+			</view>
+			<view style="margin-top:40upx">
+				<button   type="primary" @click="loginBtn()">登录</button>
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		onLoad() {
+			console.log('登录页面')
+
+		},
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+			loginBtn() {
+				console.log('loginBtn')
+				uni.switchTab({
+					url: '../tabbar/tabbar-4/tabbar-4'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.page {
+		margin: 30upx;
+	}
+
+	.content {}
+
+	.form-item-title {
+		font-weight: 600;
+		text-align: center;
+		line-height: 80upx;
+	}
+
+	.form-item {
+		display: flex;
+		height: 80upx;
+		line-height: 80upx;
+	}
+
+	.form-item-left {
+		width: 120upx;
+		text-align: right;
+		margin-right: 40upx;
+	}
+
+	.form-item-right {
+		flex: 1;
+
+		input {
+			line-height: 80upx;
+			height: 80upx;
+			padding: 0upx;
+		}
+	}
+
+	.form-item-buttom {
+		border-bottom: 1px solid #ccc;
+	}
+
+	.form-item-top {
+		border-top: 1px solid #ccc;
+	}
+</style>
---
+++ pages/tabbar/tabbar-4/tabbar-4.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="page">
+		<view class="page-top">
+			<view class="page-top-left">
+				<image style="width: 100px; height: 100px; background-color: #eeeeee;" :mode="mode" :src="headUrl" ></image>
+			</view>
+			<view class="page-top-right">
+				<button style="width:200upx" type="default" @click="gotoLogin()">点击登录</button>
+			</view>
+			
+		</view>
+		
+		 
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello' ,
+				mode:'aspectFit',
+				headUrl:'https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2319772070,3114389419&fm=26&gp=0.jpg'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			gotoLogin(){
+				uni.navigateTo({
+					url:'../../login/index'
+				})
+			}
+
+		}
+	}
+</script>
+
+<style>
+	.page{
+		margin: 20upx;
+	}
+	.page-top{
+		display: flex;
+	}
+	.page-top-left{
+		flex: 1;
+	}
+	.page-top-right{
+		flex: 3;
+		text-align: left;
+	}
+</style>
