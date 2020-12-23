40e2dc888603d722b73aa9caf1ec2617033dfe49
POLYSWEET\zhangyuchi <zhangyuchi@polyihome.com>
2020-12-23 11:43:26 +0800

    登录页面，加上手机验证码功能。

--- src/pages/login/index/index.vue
+++ src/pages/login/index/index.vue
@@ -4,13 +4,13 @@
  * @Author: zyc
  * @Date: 2020-10-29 15:58:19
  * @LastEditors: zyc
- * @LastEditTime: 2020-12-11 10:54:32
+ * @LastEditTime: 2020-12-23 11:42:40
 -->
 <template>
   <view class="page login-page-style">
     <view style="margin: 50rpx"></view>
     <view class="img-logo">
-      <image src="../../../static/login/1.png" mode="scaleToFill"></image>
+      <image src="../../../static/login/logo.png" mode="scaleToFill"></image>
     </view>
     <view style="margin: 70rpx"></view>
 
@@ -21,33 +21,101 @@
         :current="current"
         @change="changeType"
       ></u-tabs>
-      <u-form :model="form" ref="uForm">
-        <u-form-item class="login-form-item" prop="account">
-          <u-icon name="account" class="account-ico"></u-icon>
-          <u-input
-            class="account-input"
+      <view class="form-container" v-show="current == 0">
+        <view class="form-container-item">
+          <u-icon
+            name="account"
+            class="account-ico form-container-item-ico"
+          ></u-icon>
+          <input
+            v-model="phoneForm.phone"
+            class="uni-input form-container-item-input"
+            placeholder="手机号码"
+            maxlength="11"
+          />
+        </view>
+        <view class="form-container-item">
+          <u-row gutter="12">
+            <u-col span="7">
+              <u-icon
+                name="lock"
+                class="password-ico form-container-item-ico"
+              ></u-icon>
+              <input
+                v-model="phoneForm.code"
+                type="number"
+                class="uni-input form-container-item-input"
+                placeholder="验证码"
+                maxlength="6"
+              />
+            </u-col>
+            <u-col span="5">
+              <u-button
+                class="form-container-item-phone"
+                type="primary"
+                shape="circle"
+                :plain="true"
+                :disabled="second != 60"
+                @click="sendCode()"
+                >{{ codeBtnText }}</u-button
+              >
+            </u-col>
+          </u-row>
+        </view>
+        <view style="margin-top: 60rpx">
+          <u-button type="primary" shape="circle" @click="loginPhone()"
+            >登 录</u-button
+          >
+        </view>
+      </view>
+      <view class="form-container" v-show="current == 1">
+        <view class="form-container-item">
+          <u-icon
+            name="account"
+            class="account-ico form-container-item-ico"
+          ></u-icon>
+          <input
             v-model="form.account"
+            class="uni-input form-container-item-input"
             placeholder="账号"
           />
-        </u-form-item>
-        <u-form-item class="login-form-item" prop="password">
-          <u-icon name="lock" class="password-ico"></u-icon>
-          <u-input
-            class="password-input"
-            type="password"
-            v-model="form.password"
-            placeholder="密码"
-          />
-        </u-form-item>
-      </u-form>
-      <view style="margin: 80rpx"></view>
-      <u-button type="primary" @click="submitUser">登录</u-button>
+        </view>
+        <view class="form-container-item">
+          <u-row gutter="12">
+            <u-col span="11">
+              <u-icon
+                name="lock"
+                class="password-ico form-container-item-ico"
+              ></u-icon>
+              <input
+                v-model="form.password"
+                class="uni-input form-container-item-input"
+                placeholder="密码"
+                :password="passwordType"
+              />
+            </u-col>
+            <u-col span="1">
+              <image
+                class="form-container-item-eye"
+                src="../../../static/login/eye.png"
+                mode="scaleToFill"
+                @click="eye()"
+              ></image>
+            </u-col>
+          </u-row>
+        </view>
+        <view style="margin-top: 60rpx">
+          <u-button type="primary" shape="circle" @click="login()"
+            >登 录</u-button
+          >
+        </view>
+      </view>
     </view>
 
     <view class="img-bottom">
       <image
         style="width: 100%"
-        src="../../../static/login/5.png"
+        src="../../../static/login/bottom.png"
         mode="scaleToFill"
       ></image>
     </view>
@@ -55,12 +123,20 @@
 </template>
 
 <script>
-import { loginApi, getUserInfoApi } from "../../../api/index";
+import {
+  loginApi,
+  loginPhoneApi,
+  getUserInfoApi,
+  getSessionUserSendSmsApi,
+} from "../../../api/index";
 import storageTool from "../../../common/storageTool";
 
 export default {
   data() {
     return {
+      codeBtnText: "发送验证码",
+      second: 60,
+      passwordType: true,
       list: [
         {
           name: "手机号码登录",
@@ -75,22 +151,9 @@ export default {
         account: "",
         password: "",
       },
-
-      rules: {
-        account: [
-          {
-            required: true,
-            message: "请输入账号",
-            trigger: ["change"],
-          },
-        ],
-        password: [
-          {
-            required: true,
-            message: "请输入密码",
-            trigger: ["change"],
-          },
-        ],
+      phoneForm: {
+        phone: "",
+        code: "",
       },
     };
   },
@@ -99,61 +162,121 @@ export default {
     changeType(index) {
       this.current = index;
     },
+    async login() {
+      //账号密码登录
+      console.log(this.form);
+      if (this.form.account && this.form.account.length > 0) {
+      } else {
+        uni.showToast({
+          title: "账号必填",
+          icon: "none",
+        });
+        return;
+      }
+      if (this.form.password && this.form.password.length > 0) {
+      } else {
+        uni.showToast({
+          title: "密码必填",
+          icon: "none",
+        });
+        return;
+      }
+      const res = await loginApi({
+        username: this.form.account,
+        password: this.form.password,
+      });
+      this.loginSuccess(res);
+    },
+    async loginSuccess(res) {
+      storageTool.setToken(res.access_token, res.expires_in);
+      const userInfo = await getUserInfoApi();
+      storageTool.setUserInfo(userInfo);
+      storageTool.goHome();
+    },
+    async loginPhone() {
+      //验证码登录
+      console.log(this.phoneForm);
+      if (this.phoneForm.phone && this.phoneForm.phone.length > 0) {
+      } else {
+        uni.showToast({
+          title: "手机号码必填",
+          icon: "none",
+        });
+        return;
+      }
+      if (this.phoneForm.code && this.phoneForm.code.length > 0) {
+      } else {
+        uni.showToast({
+          title: "验证码必填",
+          icon: "none",
+        });
+        return;
+      }
+      //
+      const res = await loginPhoneApi({
+        phone: this.phoneForm.phone,
+        code: this.phoneForm.code,
+      });
+      this.loginSuccess(res);
+    },
+    async sendCode() {
+      //发送验证码
+      if (this.checkPhone(this.phoneForm.phone)) {
+        const res = await getSessionUserSendSmsApi(this.phoneForm.phone);
+        console.log(res);
+        uni.showToast({
+          title: "发送成功",
+          icon: "none",
+        });
+        //倒计时
+        let timer = setInterval(() => {
+          this.second--;
+          this.codeBtnText = this.second + "秒";
+          if (this.second == 0) {
+            this.codeBtnText = "发送验证码";
+            clearInterval(timer);
+            this.second = 60;
+          }
+        }, 1000);
+      } else {
+        uni.showToast({
+          title: "手机号码格式有误",
+          icon: "none",
+        });
+      }
+    },
+    //检查手机号码格式
+    checkPhone(value) {
+      let reg = /^[1][3-9]\d{9}$/;
+      if (!reg.test(value)) {
+        return false;
+      } else {
+        return true;
+      }
+    },
 
     async submitUser() {
       const that = this;
-      this.$refs.uForm.validate(async (valid) => {
-        if (valid) {
-          const res = await loginApi({
-            username: that.form.account,
-            password: that.form.password,
-          });
-          storageTool.setToken(res.access_token, res.expires_in);
-          const userInfo = await getUserInfoApi();
-          storageTool.setUserInfo(userInfo);
-          storageTool.goHome();
-        } else {
-          console.log("验证失败");
-        }
+      const res = await loginApi({
+        username: that.form.account,
+        password: that.form.password,
       });
+      storageTool.setToken(res.access_token, res.expires_in);
+      const userInfo = await getUserInfoApi();
+      storageTool.setUserInfo(userInfo);
+      storageTool.goHome();
+    },
+    async eye() {
+      this.passwordType = !this.passwordType;
     },
   },
 
-  // 必须要在onReady生命周期，因为onLoad生命周期组件可能尚未创建完毕
-  onReady() {
-    this.$refs.uForm.setRules(this.rules);
-  },
-  created() {
-    // this.$has('xx')
-    console.log(getApp().globalData.tabList);
-    uni.showShareMenu({
-      withShareTicket: true,
-      menus: ["shareAppMessage", "shareTimeline"],
-    });
-  },
-  // onShareAppMessage(res) {
-  //   console.log(res);
-  //   //res.from   menu   button
-  //   if (res.from === "button") {
-  //     // 来自页面内分享按钮
-  //     console.log(res.target);
-  //   } else if (res.from === "menu") {
-  //     // 来自右上角分享按钮
-  //   }
-  //   return {
-  //     title: "自定义分享标题",
-  //     path: "/pages/customer/index/index?id=123",
-  //     imageUrl: "/static/logo.png",
-  //   };
-  // },
+  onReady() {},
+  created() {},
 };
 </script>
 
 <style lang="scss" scoped>
-.page {
-  // padding: 0 20rpx 20rpx 20rpx;
-}
-
 .login-select-select {
   line-height: 100rpx;
   border-bottom: 1px solid #eeeeee;
@@ -173,36 +296,9 @@ export default {
     width: 150rpx;
   }
 }
-.account-ico,
-.password-ico {
-  font-size: 40rpx;
-  color: #c2c2c2;
-  display: inline-block;
-  position: relative;
-  top: 18rpx;
-  width: 10%;
-}
 </style>
 <style lang="scss">
 .login-page-style {
-  .password-input,
-  .account-input {
-    display: inline-block;
-    width: 90%;
-  }
-  .password-input view,
-  .account-input view {
-    padding-left: 0rpx !important;
-  }
-  .login-form-item {
-    height: 100rpx !important;
-  }
-  button {
-    border-radius: 80rpx;
-  }
-  .u-form-item__message {
-    padding-left: 25px !important;
-  }
   .u-scroll-view {
     border-bottom: 1px solid #eee !important;
   }
@@ -218,6 +314,32 @@ export default {
     width: 100%;
     position: fixed;
     bottom: -5px;
+  }
+
+  .form-container {
+    margin: 0rpx 0rpx;
+  }
+  .form-container-item {
+    line-height: 46rpx;
+    padding-bottom: 40rpx;
+    border-bottom: 1px solid #dbdbdb;
+  }
+  .form-container-item-ico {
+    position: relative;
+    top: 46rpx;
+  }
+  .form-container-item-input {
+    padding-left: 60rpx;
+  }
+  .form-container-item-phone {
+    position: relative;
+    top: 30rpx;
+  }
+  .form-container-item-eye {
+    width: 60rpx;
+    height: 60rpx;
+    position: relative;
+    top: 40rpx;
   }
 }
 </style>
