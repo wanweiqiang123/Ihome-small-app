8b02b37c33ffbda66556580e8e25d771bf404f16
wanweiqiang05531 <wanweiqiang055361@polysweet.local>
2021-04-29 19:00:59 +0800

    任务: 348

--- src/staffPackage/personalTab/component/tab.vue
+++
@@ -1,8 +0,0 @@
-<!--
- * @Description: file content
- * @version: 
- * @Author: ywl
- * @Date: 2021-04-29 15:27:14
- * @LastEditors: ywl
- * @LastEditTime: 2021-04-29 15:27:14
--->
--- src/staffPackage/personalTab/component/userTool.vue
+++ src/staffPackage/personalTab/component/userTool.vue
@@ -3,8 +3,8 @@
  * @version: 
  * @Author: ywl
  * @Date: 2021-04-29 15:25:57
- * @LastEditors: ywl
- * @LastEditTime: 2021-04-29 16:27:40
+ * @LastEditors: wwq
+ * @LastEditTime: 2021-04-29 18:58:44
 -->
 <template>
   <view>
@@ -28,15 +28,105 @@
     <view class="user-info">
       <view class="item">
         <view class="label">用户姓名</view>
-        <view>{{userData.name || '-'}}</view>
+        <view style="font-size: 28rpx">{{userData.name || '-'}}</view>
       </view>
       <view class="item">
         <view class="label">岗位</view>
-        <view>{{userData.job ? (userData.job.name || '-') : '-'}}</view>
+        <view style="font-size: 28rpx">{{userData.job ? (userData.job.name || '-') : '-'}}</view>
       </view>
       <view class="item">
         <view class="label">归属组织</view>
-        <view>{{userData.org ? (userData.org.name || '-') : '-'}}</view>
+        <view style="font-size: 28rpx">{{userData.org ? (userData.org.name || '-') : '-'}}</view>
+      </view>
+    </view>
+    <view>
+      <u-subsection
+        :list="list"
+        :current="curNow"
+        @change="tabChange"
+      ></u-subsection>
+      <view v-if="curNow === 0">
+        <view class="search-title">
+          <view class="search">
+            <u-search
+              style="flex: 1"
+              placeholder="输入关键字筛选"
+              v-model="keyword1"
+              clearabled
+              :focus="true"
+              :show-action="false"
+            ></u-search>
+          </view>
+        </view>
+        <view class="search-content">
+          <scroll-view
+            scroll-y="true"
+            style="height: calc(100vh - 480rpx)"
+            lower-threshold="50"
+            v-if="roleList.length"
+          >
+            <template v-for="(item, i) in roleList">
+              <view
+                :key="i"
+                class="list-item"
+              >
+                <view>{{item.name}}</view>
+                <view>{{item.code}}</view>
+              </view>
+            </template>
+          </scroll-view>
+          <view
+            v-else
+            class="searchNull"
+          >
+            <u-empty
+              :key="i"
+              text="没有搜索结果"
+              mode="search"
+            ></u-empty>
+          </view>
+        </view>
+      </view>
+      <view v-show="curNow === 1">
+        <view class="search-title">
+          <view class="search">
+            <u-search
+              style="flex: 1"
+              placeholder="输入关键字筛选"
+              v-model="keyword2"
+              clearabled
+              :focus="true"
+              :show-action="false"
+            ></u-search>
+          </view>
+        </view>
+        <view class="search-content">
+          <scroll-view
+            style="height: calc(100vh - 480rpx)"
+            scroll-y="true"
+            lower-threshold="50"
+            v-if="permissionOrgList.length"
+          >
+            <template v-for="(item, i) in permissionOrgList">
+              <view
+                class="list-item"
+                :key="i"
+              >
+                <veiw>{{item.name}}</veiw>
+              </view>
+            </template>
+          </scroll-view>
+          <view
+            v-else
+            class="searchNull"
+          >
+            <u-empty
+              :key="i"
+              text="没有搜索结果"
+              mode="search"
+            ></u-empty>
+          </view>
+        </view>
       </view>
     </view>
   </view>
@@ -44,6 +134,7 @@
 
 <script>
 import { getSessionUserByAccount } from "../../../api/staff";
+import { getAllByTypeApi } from "../../../api/index";
 
 export default {
   name: "userTool",
@@ -57,9 +148,52 @@ export default {
         permissionOrgList: [],
       },
       account: "",
+      list: [
+        {
+          name: "功能权限",
+          code: "roleList",
+        },
+        {
+          name: "组织权限",
+          code: "permissionOrgList",
+        },
+      ],
+      keyword1: "",
+      keyword2: "",
+      curNow: 0,
+      loadingStatus: "loading",
+      OrgType: [],
     };
   },
+  computed: {
+    permissionOrgList() {
+      let arr = this.userData.permissionOrgList.filter((v, i) => {
+        return v.name.includes(this.keyword2);
+      });
+      return arr;
+    },
+    roleList() {
+      return this.userData.roleList.filter((v) => {
+        return v.name.includes(this.keyword1);
+      });
+    },
+  },
+  async onLoad() {
+    this.OrgType = await this.getDictAll("OrgType");
+  },
   methods: {
+    // 字典翻译
+    async getDictAll(type) {
+      const dictList = await getAllByTypeApi({ type });
+      return dictList;
+    },
+    // 字典匹配
+    getDictName(code, list) {
+      if (list.length) {
+        const item = list.find((v) => v.code === code);
+        return item?.name;
+      }
+    },
     async getUserInfo() {
       if (!this.account) {
         this.$tool.toast("账号名不能为空");
@@ -69,9 +203,20 @@ export default {
         this.userData = await getSessionUserByAccount({
           account: this.account,
         });
+        this.userData.permissionOrgList = this.userData.permissionOrgList.filter(
+          (v) => {
+            return ["Department", "Group"].includes(v.orgType);
+          }
+        );
       } catch (error) {
         console.log(error);
       }
+    },
+    // tab切换
+    tabChange(index) {
+      this.curNow = index;
+      this.keyword1 = "";
+      this.keyword2 = "";
     },
   },
 };
@@ -109,6 +254,37 @@ export default {
     color: #cccccc;
     padding-bottom: 20rpx;
   }
+}
+.search-title {
+  padding: 20rpx 20rpx 0 20rpx;
+  position: sticky;
+  top: 0;
+  background-color: #ffffff;
+  z-index: 10;
+}
+.search {
+  display: flex;
+  width: 100%;
+  padding-bottom: 10rpx;
+}
+.search-content {
+  border-top: 1px solid #e4e7ed;
+  padding: 0 10rpx 24rpx 24rpx;
+  text-align: center;
+  flex: 1;
+}
+.list-item {
+  padding: 20rpx;
+  border-bottom: 1px solid #e4e7ed;
+  display: flex;
+  justify-content: space-between;
+  background-color: #ffffff;
+}
+.searchNull {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%);
 }
 </style>
 <style lang="scss">
